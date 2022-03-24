ce8f6b39620f955e2733348e469d1866a7170d82
vans <menonhot@gmail.com>
2022-03-24 11:20:19 +0700

    add urls and views for demo app

--- demo/models.py
+++ demo/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=36)
+    title = models.CharField(max_length=36,blank=False, unique=True)
+    description = models.TextField(max_length=256,blank=True)
+    price = models.DecimalField(default=0,decimal_places=2,max_digits=3)
+    published = models.DateField(blank=True,null=True,default=None)
+    isPublished = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='covers/',blank=True)
